8ac2df250d8176b06cfbd7e4c1fba3a689321fba
Sef <null>
2018-09-24 21:30:02 -0400

    Use DateTimeOffset.FromUnixTimeSeconds

--- src/Lykke.Service.Decred.Api.Services/TransactionBroadcastService.cs
+++ src/Lykke.Service.Decred.Api.Services/TransactionBroadcastService.cs
@@ -18,7 +18,7 @@ namespace Lykke.Service.Decred.Api.Services
         Task UnsubscribeBroadcastedTx(Guid operationId);
         Task<BroadcastedSingleTransactionResponse> GetBroadcastedTxSingle(Guid operationId);
     }
-    
+
     public class TransactionBroadcastService : ITransactionBroadcastService
     {
         private readonly IDcrdClient _dcrdClient;
@@ -30,7 +30,7 @@ namespace Lykke.Service.Decred.Api.Services
         private readonly INosqlRepo<BroadcastedOutpoint> _broadcastedOutpointRepo;
 
         public TransactionBroadcastService(
-            IDcrdClient dcrdClient, 
+            IDcrdClient dcrdClient,
             IBlockRepository blockRepository,
             ITransactionRepository txRepo,
             INosqlRepo<BroadcastedOutpoint> broadcastedOutpointRepo,
@@ -40,7 +40,7 @@ namespace Lykke.Service.Decred.Api.Services
             _dcrdClient = dcrdClient;
             _blockRepository = blockRepository;
             _txRepo = txRepo;
-            
+
             _broadcastTxRepo = broadcastTxRepo;
             _broadcastTxHashRepo = broadcastTxHashRepo;
             _broadcastedOutpointRepo = broadcastedOutpointRepo;
@@ -57,7 +57,7 @@ namespace Lykke.Service.Decred.Api.Services
             var tx = Transaction.Deserialize(txBytes);
             return tx.Inputs.Select(input => input.PreviousOutpoint.ToString()).ToArray();
         }
-        
+
         /// <summary>
         /// Broadcasts a signed transaction to the Decred network
         /// </summary>
@@ -66,12 +66,12 @@ namespace Lykke.Service.Decred.Api.Services
         /// <returns></returns>
         /// <exception cref="TransactionBroadcastException"></exception>
         public async Task Broadcast(Guid operationId, string hexTransaction)
-        {               
+        {
             if (operationId == Guid.Empty)
                 throw new BusinessException(ErrorReason.BadRequest, "Operation id is invalid");
             if (string.IsNullOrWhiteSpace(hexTransaction))
                 throw new BusinessException(ErrorReason.BadRequest, "SignedTransaction is invalid");
-            
+
             var txBytes = HexUtil.ToByteArray(hexTransaction);
             var msgTx = new MsgTx();
             msgTx.Decode(txBytes);
@@ -80,9 +80,9 @@ namespace Lykke.Service.Decred.Api.Services
             var cachedResult = await _broadcastTxRepo.GetAsync(operationId.ToString());
             if (cachedResult != null)
                 throw new BusinessException(ErrorReason.DuplicateRecord, "Operation already broadcast");
-            
+
             // Submit the transaction to the network via dcrd
-            var result = await _dcrdClient.SendRawTransactionAsync(hexTransaction);                
+            var result = await _dcrdClient.SendRawTransactionAsync(hexTransaction);
             if (result.Error != null)
                 throw new TransactionBroadcastException($"[{result.Error.Code}] {result.Error.Message}");
 
@@ -90,16 +90,16 @@ namespace Lykke.Service.Decred.Api.Services
             var outpoints = GetOutpointKeysForRawTransaction(txBytes);
             foreach (var outpoint in outpoints)
                 _broadcastedOutpointRepo.InsertAsync(new BroadcastedOutpoint {Value = outpoint});
-            
+
             var txHash = HexUtil.FromByteArray(msgTx.GetHash().Reverse().ToArray());
             await SaveBroadcastedTransaction(new BroadcastedTransaction
             {
                 OperationId = operationId,
                 Hash = txHash,
                 EncodedTransaction = hexTransaction
-            });   
+            });
         }
-        
+
         /// <summary>
         /// Determines the state of a broadcasted transaction
         /// </summary>
@@ -115,23 +115,23 @@ namespace Lykke.Service.Decred.Api.Services
             var broadcastedTransaction = await GetBroadcastedTransaction(operationId);
             var transaction = new MsgTx();
             transaction.Decode(HexUtil.ToByteArray(broadcastedTransaction.EncodedTransaction));
-            
+
             // Calculate the fee and total amount spent from the transaction.
             var fee = transaction.TxIn.Sum(t => t.ValueIn) - transaction.TxOut.Sum(t => t.Value);
             var amount = transaction.TxOut.Sum(t => t.Value);
-            
+
             // Check to see if the transaction has been included in a block.
             var safeBlockHeight = await _dcrdClient.GetMaxConfirmedBlockHeight();
             var knownTx = await _txRepo.GetTxInfoByHash(broadcastedTransaction.Hash, safeBlockHeight);
             var txState = knownTx == null
                 ? BroadcastedTransactionState.InProgress
                 : BroadcastedTransactionState.Completed;
-            
+
             // If the tx has been included in a block,
             // use the block height + timestamp from the block
             var txBlockHeight = knownTx?.BlockHeight ?? safeBlockHeight;
-            var timestamp = knownTx == null ? DateTime.UtcNow : DateTimeUtil.FromUnixTime(knownTx.BlockTime);
-            
+            var timestamp = knownTx == null ? DateTime.UtcNow : DateTimeOffset.FromUnixTimeSeconds(knownTx.BlockTime).UtcDateTime;
+
             return new BroadcastedSingleTransactionResponse
             {
                 Block = txBlockHeight,
@@ -154,17 +154,17 @@ namespace Lykke.Service.Decred.Api.Services
             var operation = await _broadcastTxRepo.GetAsync(operationId.ToString());
             if (operation == null)
                 throw new BusinessException(ErrorReason.RecordNotFound, "Record not found");
-            
+
             // Unflag outpoints as spent.
             var outpoints = GetOutpointKeysForRawTransaction(operation.EncodedTransaction);
             foreach (var outpoint in outpoints)
                 await _broadcastedOutpointRepo.DeleteAsync(new BroadcastedOutpoint{Value = outpoint});
-    
+
             await _broadcastTxRepo.DeleteAsync(operation);
         }
-        
+
         private async Task SaveBroadcastedTransaction(BroadcastedTransaction broadcastedTx)
-        {            
+        {
             // Store tx Hash to OperationId lookup
             await _broadcastTxHashRepo.InsertAsync(
                 new BroadcastedTransactionByHash
