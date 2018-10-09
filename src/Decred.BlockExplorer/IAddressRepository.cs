b127b26f7dce7cb2c16142c58a2f91040d2b5c3a
Sef <null>
2018-10-08 22:32:24 -0400

    Updated comments

--- src/Decred.BlockExplorer/IAddressRepository.cs
+++ src/Decred.BlockExplorer/IAddressRepository.cs
@@ -7,9 +7,9 @@ namespace Decred.BlockExplorer
         /// <summary>
         /// Determines the unspent balance of each address at a point in time.
         /// </summary>
-        /// <param name="maxBlockHeight"></param>
-        /// <param name="addresses"></param>
-        /// <returns></returns>
+        /// <param name="maxBlockHeight">Maximum block height to scan up to</param>
+        /// <param name="addresses">List of addresses to retrive balances for</param>
+        /// <returns>Collection of address balances.</returns>
         Task<AddressBalance[]> GetAddressBalancesAsync(string[] addresses, long blockHeight);
     }
 }
